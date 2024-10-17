b20346872ab95fabbe4d6f97087c929e29fa149a
Nicolas <nicolas.ciriza@etu.hesge.ch>
2024-10-17 17:49:00 +0300

    Add main components and navigation with react-native-paper

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { BottomNavigation, PaperProvider } from 'react-native-paper';
+import RecipeSearch from './components/RecipeSearch';
+import GroceryList from './components/GroceryList';
+import FavoriteMeals from './components/FavoriteMeals';
 
 export default function App() {
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'recipeSearch', title: 'Recipes', focusedIcon: 'text-box-search', unfocusedIcon: 'text-box-search-outline' },
+    { key: 'favoriteMeals', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline' },
+    { key: 'groceryList', title: 'Groceries', focusedIcon: 'cart', unfocusedIcon: 'cart-outline' },
+  ]);
+
+  // Map routes with components
+  const renderScene = BottomNavigation.SceneMap({
+    recipeSearch: RecipeSearch,
+    favoriteMeals: FavoriteMeals,
+    groceryList: GroceryList,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
